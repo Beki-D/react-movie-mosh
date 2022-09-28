0af7b00ad5fc671c1564d7dcc454e2151c3e37c0
beki-d <bk14drobs@gmail.com>
2022-09-28 16:21:19 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import "./index.css";
+import App from "./App";
+import 'bootstrap/dist/css/bootstrap.css'
+import Counter from "./components/counter"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('root');
+const root = createRoot(container);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Counter tab="home"/>);
+//ReactDOM.render(<Counter />, document.getElementById("root"));
