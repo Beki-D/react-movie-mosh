e300c116e4c26208b7f7859aa540d1f3ee621f10
beki-d <bk14drobs@gmail.com>
2022-10-01 10:57:08 +0300

    Almost done

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,7 +3,7 @@
 //stateless functional component
 const NavBar = ({totalCounters}) => {
     return (
-        <nav className="navbar navbar-light bg-light">
+        <nav className="navbar navbar-dark bg-dark mx-auto">
             <a className="navbar-brand" href="#navbar-brand">
                 Navbar <span className="badge badge-pill bg-secondary">{totalCounters}</span>
             </a>
