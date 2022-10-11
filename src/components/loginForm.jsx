d875782f60fa7e06b8adce611a0be236ca71def9
beki-d <bk14drobs@gmail.com>
2022-10-11 12:45:06 +0300

    Made major refactoring on the form,specially input

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,74 +1,22 @@
-import React, { Component } from 'react';
 import Joi from 'joi-browser';
-import Input from './common/input';
 import InfoCard from './infoCard';
+import Form from './common/form'
 
-class LoginForm extends Component {
+class LoginForm extends Form {
     state = {
-        account: { username: '', password: '' },
+        data: { username: '', password: '' },
         errors: { },
         passwordVisible : false
     };
-    
-    //To create a reference
-    // username = React.createRef();
-    //Put ref in the element
-    // <input ref={this.username}.../>
 
     schema = {
         username: Joi.string().required().label("Username"),
         password: Joi.string().required().label("Password")
     }
-
-    
-    // const errors = {};
-
-    // const { account } = this.state;
-    // if (account.username.trim() === "")
-    //     errors.username = "Username is required";
-    // if (account.password.trim() === "")
-    //     errors.password = "Password is required";
-    // return Object.keys(errors).length === 0 ? null : errors;
-
-    validate = () => {
-        const options = { abortEarly: false };
-        const { error } = Joi.validate(this.state.account, this.schema, options);
-        if(!error) return null;
-
-        const errors = {};
-        for (let item of error.details) errors[item.path[0]] = item.message;
-        return errors;
-    }
-    
-    validateProperty = ({name, value}) => {
-        const obj = { username: value};
-        const schema = {[name]: this.schema[name]};
-        const {error} = Joi.validate(obj, schema);
-        return error ? error.details[0].message : null;
-    }
-    
-    handleSubmit = (e) => {
-        e.preventDefault();
-
-        const errors = this.validate();
-        this.setState({ errors: errors || {} });
-        if(errors) return;
-
+   
+    doSubmit = () => {
         //Call server
         console.log('Submitted successfully');
-    ;}
-
-
-    handleChange = ({ currentTarget: input}) => {
-        const errors = {...this.state.errors};
-        const errorMessage = this.validateProperty(input);
-        if(errorMessage) errors[input.name] = errorMessage;
-        else delete errors[input.name];
-
-        const account = {...this.state.account};
-        account[input.name] = input.value;
-
-        this.setState({account, errors});
     }
 
     handleClick = () => {
@@ -77,37 +25,16 @@ class LoginForm extends Component {
     }
 
     render() { 
-        const { account, passwordVisible, errors } = this.state;
-
         return (
             <div className="row">
                 <div className="col-sm-5 col-md-6 mt-5">
                     <h1>Login</h1>
                     <form onSubmit={this.handleSubmit}>
-                        <Input 
-                            name="username" 
-                            value={account.username}
-                            label="Username"
-                            onChange={this.handleChange}
-                            visibility={true}
-                            error={errors.username}
-                        />
+                        {this.renderInput("username", "Username", true)}
                         <div className="mb-3">
-                            <Input
-                                name="password"
-                                value={account.password}
-                                label="password"
-                                onChange={this.handleChange}
-                                visibility={passwordVisible}
-                                handleClick={this.handleClick}
-                                error={errors.password}
-                            />
+                            {this.renderInput("password", "Password")}
                         </div>
-                        <button 
-                            disabled={this.validate()}
-                            className="btn btn-info mb-3">
-                            Login
-                        </button>
+                        {this.renderButton("Login")}
                     </form>
                 </div>
                 <InfoCard />
